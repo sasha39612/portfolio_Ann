e735df81927486f7cd61814c73e049ca93f1c93f
Oleksandr Stolyarov <stolyarov_396@icloud.com>
2026-09-06 11:29:39 +0200

    Fixed styles

--- components/ui/popup/Popup.tsx
+++ components/ui/popup/Popup.tsx
@@ -15,10 +15,13 @@ const Popup = ({ children, onClose }: PopupProps) => {
     if (portalRoot) {
       portalRoot.appendChild(el);
     }
+    const previousOverflow = document.body.style.overflow;
+    document.body.style.overflow = 'hidden';
     return () => {
       if (portalRoot) {
         portalRoot.removeChild(el);
       }
+      document.body.style.overflow = previousOverflow;
     };
   }, [el]);
 
